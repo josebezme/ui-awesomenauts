3044726e3cc02a120c62e3037e4c8fcc9fdd026b
SquareWatermelon <rmt2131@columbia.edu>
2012-12-07 18:20:16 -0500

    Added Yelp Wrapper and a test. Please give the wrapper a query, a location (such as zipcode), and the function you want executed with the data once it is returned. This function must take one parameter which the yelp wrapper will give the data in. An example on this is given in the test file. This wonkey way of doing things is required (or best practices) do to asyncronized execution. Not knowing this is why it took me so long to write the code.

---
+++ js/yelp-wrapper.js
@@ -0,0 +1,69 @@
+/**
+ * @author Robert
+ */
+
+var auth = {
+	consumerKey: "b83be9DX7E1CK7V4WOUu6g", 
+	consumerSecret: "X-vEK7f8C9Gtqn_XW6vFcAKv88c",
+	accessToken: "bFcGq4Z2UAIeKY4VV5c5XjTrWdtiuKbv",
+	accessTokenSecret: "rQbtXS60U2wWb0bVXBwsjqi7hzE",
+	serviceProvider: { 
+		signatureMethod: "HMAC-SHA1"
+	}
+};
+
+var accessor = {
+		consumerSecret: auth.consumerSecret,
+		tokenSecret: auth.accessTokenSecret
+};
+
+/*
+ * The Main function
+ * Takes a query a zip and a 
+ */
+function searchAndDo(query, zip, fun){
+	var parameters = getParameters( query, zip );
+	var message = { 
+		'action': 'http://api.yelp.com/v2/search',
+		'method': 'GET',
+		'parameters': parameters 
+	};
+	OAuth.setTimestampAndNonce(message);
+	OAuth.SignatureMethod.sign(message, accessor);
+	var parameterMap = OAuth.getParameterMap(message.parameters);
+	parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+	console.log(parameterMap);
+
+	query += " food restaurant ";
+	
+	function queryYelp(
+		    fun) {
+		        return $.ajax({
+		        'url': message.action,
+		        'data': parameterMap,
+		        'cache': true,
+		        'dataType': 'jsonp',
+		        'jsonpCallback': 'cb',
+		    })
+		    .done(fun)
+		    .fail(function(jqXHR, textStatus, errorThrown) {
+		    	alert ('Yelp has failed to return data');
+		    });
+	}
+	queryYelp(fun);
+}
+
+/*
+ * Create the parameters to call Yelp with
+ */
+function getParameters( query, zip ){
+	var para = [];
+	para.push(['term', query]);
+	para.push(['location', zip]);
+	para.push(['callback', 'cb']);
+	para.push(['oauth_consumer_key', auth.consumerKey]);
+	para.push(['oauth_consumer_secret', auth.consumerSecret]);
+	para.push(['oauth_token', auth.accessToken]);
+	para.push(['oauth_signature_method', 'HMAC-SHA1']);
+	return para;
+}
